1d7002c37efbfdb92a2c714310e6fddbfc2f9106
Damian Zamroczynski <damianzamroczynski@gmail.com>
2025-09-04 16:57:12 +0200

    Update authorize() in ProductRequest

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -12,7 +12,7 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return $this->user()->can('Edycja Produktów');
     }
 
     /**
